1fc5e946f672d0bda96b38b09748de6e97ed90cd
Camilo Andrés Rodríguez Garzón <cam_rod@hotmail.com>
2021-11-21 17:58:45 -0500

    [added main login code]

---
+++ app/src/main/java/com/buildreams/mathvoice/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.buildreams.mathvoice
+
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.buildreams.mathvoice.composable.LoginPage
+import com.buildreams.mathvoice.ui.theme.MathVoiceTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
+        setContent {
+            MathVoiceTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    LoginComposable()
+                }
+            }
+        }
+    }
+
+    @Composable
+    private fun LoginComposable() {
+        LoginPage()
+    }
+}
